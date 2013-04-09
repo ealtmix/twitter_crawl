c6852b7fb26952f446a4a56bd570dce758613b2f
ealtmix <evan-altmix@uiowa.edu>
2013-04-08 20:00:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crawl::Application.config.secret_token = '5b5b36e8ccb38ecb203df0b0fe2f34dceb1b71bc85623c5a634bb2d1255bd42fdafdcfbc629baf2a21ff7af1a1bd30ff155de78abc31128802e6f21ef201fa3f'
