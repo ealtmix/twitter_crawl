21f955f1b1c665c87529a4d867b9766ce3ad99c1
saasbook <saasbook@saasbook.(none)>
2013-04-10 19:10:35 -0700

    test

---
+++ app/controllers/crawls_controller.rb
@@ -0,0 +1,29 @@
+class CrawlsController < ApplicationController
+
+  def show
+  
+  end
+
+  def index
+    
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
