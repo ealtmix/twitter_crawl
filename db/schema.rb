7ee811371db67411afaec660366c83c696b3c1ca
ealtmix <evan-altmix@uiowa.edu>
2013-04-20 16:23:16 -0500

    database changes, back to one table

--- db/schema.rb
+++ db/schema.rb
@@ -15,42 +15,8 @@ ActiveRecord::Schema.define(:version => 20130419164949) do
 
   create_table "crawls", :force => true do |t|
     t.text     "tweet"
+    t.text     "region"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "user"
-    t.float    "lat"
-    t.float    "long"
-  end
-  create_table "NewYork" do |t|
-    t.text :tweet
-    t.timestamps
-  end
-  create_table "SanFran" do |t|
-    t.text :tweet
-    t.timestamps
-  end
-  create_table "ChiTown" do |t|
-    t.text :tweet
-    t.timestamps
-  end
-  create_table "Atlanta" do |t|
-    t.text :tweet
-    t.timestamps
-  end
-  create_table "Denver" do |t| 
-    t.text :tweet
-    t.timestamps
-  end
-  create_table "Sea_Van" do |t|
-    t.text :tweet
-    t.timestamps
-  end
-  create_table "Texas" do |t|
-    t.text :tweet
-    t.timestamps
-  end
-  create_table "Florida" do |t|
-    t.text :tweet
-    t.timestamps
   end
 end
