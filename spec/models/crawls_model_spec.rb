f3736577f35410c7bc12057db9284004e3232e27
Eric Horn <eric-horn@uiowa.edu>
2013-04-22 18:30:33 -0700

    working on rspec

--- spec/models/crawls_model_spec.rb
+++ spec/models/crawls_model_spec.rb
@@ -4,8 +4,7 @@ describe CrawlsModel do
   describe 'Change keyword:' do
     it 'should call the change_key_word method' do
       #Crawls.should_receive(:change_key_word.with('happy'))
-      asdf = Factory(:Crawls, a1: "happy")
-      Crawls.change_key_word.should == "happy"
+      Crawls.tweet_pruner.should
     end
   end
 end
