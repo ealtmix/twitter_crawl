5152dc6a59e09975e290ad3000a2e98866fa7da6
Eric Horn <eric-horn@uiowa.edu>
2013-04-24 17:16:37 -0700

    working on rspec

--- spec/models/crawls_model_spec.rb
+++ spec/models/crawls_model_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe CrawlsModel do
   describe 'Change keyword:' do
     it 'should call the change_key_word method' do
-      #Crawls.should_receive(:change_key_word.with('happy'))
-      Crawls.tweet_pruner.should
+      Crawls.should_receive(:tweet_pruner)
     end
   end
 end
