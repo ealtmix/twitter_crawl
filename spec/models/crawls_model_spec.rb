01b15ebb12e5ace1ee92ac668cdc02f2a7ee91a5
ealtmix <evan-altmix@uiowa.edu>
2013-04-24 20:44:16 -0500

    still trying

--- spec/models/crawls_model_spec.rb
+++ spec/models/crawls_model_spec.rb
@@ -1,14 +1,6 @@
 require 'spec_helper'
 
 describe Crawls do
-  #describe 'Change keyword:' do
-    it 'should call the change_key_word method' do
-      Crawls.should_receive(:tweet_pruner)
-      
-    end
-  #end
-end
-describe Crawls do
   it 'should return a hash of tweets from the get_region_stats method' do
     Crawls.create(:region => 'NewYork', :tweet => 'tweet')
     Crawls.get_region_stats('tweet','NewYork').is_a?(Hash)
